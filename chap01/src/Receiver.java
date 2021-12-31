ae19272027e6457a28d2843093aea69fdcd74eb7
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-12-31 12:35:43 +0000

    밸류타입의 equals 구현시 엔티티와의 차이. (엔티티는 식별자만, 밸류는 모든 값 비교)

--- chap01/src/Receiver.java
+++ chap01/src/Receiver.java
@@ -1,14 +1,27 @@
 public class Receiver {
     private String name;
     private String phoneNumber;
+
     public Receiver(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
+
     public String getName() {
         return name;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    // 밸류타입인 Receiver는 엔티티와 다르게 모든 값을 비교한다.
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (this == other) return true; // 메모리 주소값 비교
+        if (!(other instanceof Receiver)) return false;
+        Receiver that = (Receiver) other;
+        return this.name.equals(that.name) &&
+                this.phoneNumber.equals(that.phoneNumber);
     }
 }
