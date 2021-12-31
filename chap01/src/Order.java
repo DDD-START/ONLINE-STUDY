ec3da0585259cf386b6723fb699eef07c82b3b98
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-12-31 12:49:59 +0000

    습관적 setter 메서드 추가 자제: 주문 엔티티 필수 값들을 생성시점 유효성 검사. (이로써 주문자가 없는 주문을 막을 수 있게 되었다). 주문 엔티티 모든 setter의 접근범위는 private 이다.

--- chap01/src/Order.java
+++ chap01/src/Order.java
@@ -13,34 +13,27 @@ import java.util.List;
 9. 고객이 결제를 완료 하기 전에는 상품을 준비 하지 않는다 .
 * */
 public class Order {
+    private OrderNo id; // 엔티티 식별자
+    private Orderer orderer;
     private OrderState state;
     private ShippingInfo shippingInfo;
     private List<OrderLine> orderLines;
     private Money totalAmounts;
 
-    private OrderNo id; // 엔티티 식별자
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj== null) return false;
-        if (obj.getClass() != Order.class) return false;
-        Order other = (Order)obj;
-        if (this.id == null) return false;
-        return this.id.equals(other.id);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
+    public Order(Orderer orderer, List<OrderLine> orderLines, ShippingInfo shippingInfo, OrderState state) {
+        setOrderer(orderer);
         setOrderLines(orderLines);
-        setShippingInfo(shippingInfo); //
+        setShippingInfo(shippingInfo);
+        setState(state);
+    }
+
+    private void setOrderer(Orderer orderer) {
+        if(orderer == null) throw new IllegalArgumentException("no orderer info");
+        this.orderer = orderer;
+    }
+
+    private void setState(OrderState state) {
+        this.state = state;
     }
 
     private void setShippingInfo(ShippingInfo shippingInfo) {
@@ -94,5 +87,23 @@ public class Order {
     // 4 결제완료로 변경하기
     public void completePayment() {
         // ...
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (obj.getClass() != Order.class) return false;
+        Order other = (Order) obj;
+        if (this.id == null) return false;
+        return this.id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 }
