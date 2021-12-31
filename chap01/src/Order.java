1cfaf0af245affb5f9c3c1d5e59517289ae5600e
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-12-31 12:19:48 +0000

    엔티티 식별자 추가와 식별자를 이용한 equals와 hashCode 구현

--- chap01/src/Order.java
+++ chap01/src/Order.java
@@ -18,6 +18,26 @@ public class Order {
     private List<OrderLine> orderLines;
     private int totalAmounts;
 
+    private String orderNumber; // 엔티티 식별자
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj== null) return false;
+        if (obj.getClass() != Order.class) return false;
+        Order other = (Order)obj;
+        if (this.orderNumber == null) return false;
+        return this.orderNumber.equals(other.orderNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((orderNumber == null) ? 0 : orderNumber.hashCode());
+        return result;
+    }
+
     public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
         setOrderLines(orderLines);
         setShippingInfo(shippingInfo); //
