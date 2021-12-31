4c551d089006f14bc826fcb0be63295386ecd935
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-12-31 11:37:02 +0000

    리팩토링: OrderState는 Order에 속한 데이터이다. 즉, 배송지 변경 가능 여부 판단 코드는 Order로 이동 가능.

--- chap01/src/Order.java
+++ chap01/src/Order.java
@@ -3,10 +3,15 @@ public class Order {
     private ShippingInfo shippingInfo;
 
     public void changeShippingInfo(ShippingInfo newShippingInfo) { // 규칙1: 배송지 변경
-        if (!state.isShippingChangeable()) {
+        if (!isShippingChangeable()) {
             throw new IllegalStateException("can’t change shipping in" + state);
         }
         this.shippingInfo = newShippingInfo;
+    }
+
+    private boolean isShippingChangeable() {
+        return state == OrderState.PAYMENT_WAITING ||
+                state == OrderState.PREPARING;
     }
 
     public void changeShipped() {
